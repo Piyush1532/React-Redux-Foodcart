9dd922ae37e5f4302969857a9a9839883a947bd6
Piyush1532 <piyushadav503@gmail.com>
2024-08-24 13:51:56 +0530

    updated

--- src/Components/Redux/FoodSlice.jsx
+++ src/Components/Redux/FoodSlice.jsx
@@ -17,12 +17,42 @@ const Foodslice = createSlice({
         state.FoodData = action.payload;
       },
     
-handleCart(state,action){
-// console.log(action)
-if(action.payload.opt==="Add"){
-  state.cart.push(action.payload.food)
+// handleCart(state,action){
+
+// if(action.payload.opt==="Add"){
+//   const isFound=state.cart.find((cartitem)=>{
+// return cartitem.id === action.payload.cart.id
+//   })
+//   if (isFound>-1) {
+//     state.cart[isFound].qty+=1
+//   }
+//   else{
+//     let foodproduct={...action.payload.cartitem}
+//    foodproduct["qty"]=1
+//    state.cart.push(foodproduct)
+//   }
+// }
+// }
+handleCart(state, action) {
+  if (action.payload.opt === "Add") {
+    // Check if action.payload.food exists
+    if (action.payload.food) {
+      const isFound = state.cart.find((cartitem) => cartitem.id === action.payload.food.id);
+
+      if (isFound > -1) {
+        state.cart[isFound].qty += 1;
+      } else {
+        let foodproduct = { ...action.payload.food };
+        foodproduct["qty"] = 1;
+        state.cart.push(foodproduct);
+      }
+    } else {
+      console.error("Food is undefined", action.payload);
+    }
+  }
 }
-}
+
+
       
     }
   });
