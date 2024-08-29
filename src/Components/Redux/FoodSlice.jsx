ae5664ad4314d629c6979b280204654e87360e45
Piyush1532 <piyushadav503@gmail.com>
2024-08-29 13:58:23 +0530

    updated the reducer function

--- src/Components/Redux/FoodSlice.jsx
+++ src/Components/Redux/FoodSlice.jsx
@@ -5,7 +5,7 @@ const Foodslice = createSlice({
     initialState: {
       categories: [],
       FoodData: [],
-      cart: [],
+      carts: [],
     },
     reducers: {
       foodCategories(state, action) {
@@ -17,34 +17,19 @@ const Foodslice = createSlice({
         state.FoodData = action.payload;
       },
     
-// handleCart(state,action){
 
-// if(action.payload.opt==="Add"){
-//   const isFound=state.cart.find((cartitem)=>{
-// return cartitem.id === action.payload.cart.id
-//   })
-//   if (isFound>-1) {
-//     state.cart[isFound].qty+=1
-//   }
-//   else{
-//     let foodproduct={...action.payload.cartitem}
-//    foodproduct["qty"]=1
-//    state.cart.push(foodproduct)
-//   }
-// }
-// }
 handleCart(state, action) {
   if (action.payload.opt === "Add") {
-    // Check if action.payload.food exists
     if (action.payload.food) {
-      const isFound = state.cart.find((cartitem) => cartitem.id === action.payload.food.id);
+      const isFound = state.carts.findIndex((cartitem) => cartitem.id === action.payload.food.id)
 
       if (isFound > -1) {
-        state.cart[isFound].qty += 1;
+        state.carts[isFound].qty +=1;
       } else {
         let foodproduct = { ...action.payload.food };
         foodproduct["qty"] = 1;
-        state.cart.push(foodproduct);
+        state.carts.push(foodproduct);
+        console.log(foodproduct)
       }
     } else {
       console.error("Food is undefined", action.payload);
